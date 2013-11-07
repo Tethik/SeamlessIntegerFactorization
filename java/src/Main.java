e422690a96c0b49a0e21e68402e184a8915d8fb2
Joakim Uddholm <joakim@uddholm.com>
2013-11-08 00:11:06 +0100

    SqrRoot function using newton Raphson

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -13,6 +15,7 @@ public class Main {
 	private static BigInteger ZERO = new BigInteger("0");
 	private static BigInteger ONE = new BigInteger("1");
 	private static BigInteger TWO = new BigInteger("2");
+	private static BigInteger THREE = new BigInteger("3");
 	
 	
 	public static BigInteger pollardRho(BigInteger value) {
@@ -22,6 +25,9 @@ public class Main {
 	public static BigInteger pollardRho(BigInteger value, int i) {
 		if(value.mod(TWO).equals(ZERO))
 			return TWO;
+		
+		if(value.mod(THREE).equals(ZERO))
+			return THREE;
 		
 		BigInteger c =  new BigInteger(Integer.toString(i));
 		BigInteger x = new BigInteger("2").add(c);
@@ -52,7 +58,6 @@ public class Main {
 		BigInteger quo = value;
 		
 		while(!quo.isProbablePrime(20)) {
-//			System.out.println("derp1");
 			BigInteger factor = null;
 			for(int i = 0; ; i++) {
 				factor = pollardRho(quo, i);				
@@ -70,6 +75,38 @@ public class Main {
 		factors.add(quo);
 	}
 	
+	/**
+	 * Finds root with exponent k of given value. 
+	 * Returns null on failure.
+	 * @param value
+	 * @param k
+	 * @return
+	 */
+	public static BigInteger getPowRoot(BigInteger value, int k) {
+		BigDecimal converted_value = new BigDecimal(value.toString());
+		//new BigDecimal(value.toString());
+		BigDecimal x = new BigDecimal("2").pow(value.bitLength() / k);		
+		BigDecimal fx;
+		BigDecimal fprimx;
+		BigDecimal xold;
+		
+		// Newton-Raphson
+		// x - f(x) / f'(x)
+		// for function: x2 - n = 0
+
+		do {
+			xold = x;
+			fx = xold.pow(k).subtract(converted_value);				
+			fprimx = xold.pow(k-1).multiply(BigDecimal.valueOf(k));
+			x = xold.subtract(fx.divide(fprimx, RoundingMode.HALF_DOWN));
+//			System.out.println(x + " " + xold + " " + x.subtract(xold) + " " + x.pow(k));
+		} while(x.subtract(xold).abs().compareTo(BigDecimal.ONE) >= 0);
+		
+		if(x.pow(k).equals(converted_value))
+			return x.toBigIntegerExact();
+		return null;
+	}
+	
 	private static long timeout_when = 0;
 	private static Queue<BigInteger> factors = new LinkedList<BigInteger>(); 
 	
@@ -78,6 +115,8 @@ public class Main {
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
+//		System.out.println(getPowRoot(new BigInteger("63"), 3));
+		
 		ArrayList<BigInteger> values = new ArrayList<BigInteger>();
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		while(true) {
@@ -88,17 +127,34 @@ public class Main {
 			values.add(new BigInteger(line));
 		}
 		
-	long start_time = System.currentTimeMillis();
-	long stop_time = start_time + TIMEOUT;
-		
-	int values_to_go = values.size();
+		long start_time = System.currentTimeMillis();
+		long stop_time = start_time + TIMEOUT;
+			
+		int values_to_go = values.size();
 		int iWin = 0;
 		for(BigInteger value : values) {
 			factors.clear();
+			Queue<BigInteger> toFactor = new LinkedList<BigInteger>();
 			long currtime = System.currentTimeMillis();
-			timeout_when =  currtime + (stop_time - currtime) / values_to_go;
-//			System.out.println(timeout_when);
-			factor(value);
+			timeout_when =  currtime + ((stop_time - currtime) / values_to_go);
+
+			// Potenser. Sätt k = 6 till något vettigare senare.
+			for(int k = 6; k > 1; k--) {
+				BigInteger root = getPowRoot(value, k);
+				if(root != null) {
+					System.err.println(root);
+					if(root.isProbablePrime(20))
+						factors.add(root);
+					else
+						toFactor.add(root);
+					value = value.divide(root);
+					k = 2;
+				}
+			}
+			toFactor.add(value);
+			
+			for(BigInteger toFac : toFactor)
+				factor(toFac);
 			
 			if(timeout_when <= System.currentTimeMillis()) {
 				System.out.println("fail");
